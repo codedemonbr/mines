98b01bb0c9dd1ff1f995bc12fbc8fa67aa16fd36
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-22 19:19:05 -0300

    Criado o component Field

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import params from "./src/params";
+import Field from "./src/components/Field";
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
             <Text>Iniciando o Mines!</Text>
-            <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+            <Text>
+                Tamanho da grade: {params.getRowsAmount()}x
+                {params.getColumnsAmount()}
+            </Text>
+            <Field />
+            <Field opened />
+            <Field opened nearMines={1} />
+            <Field opened nearMines={2} />
+            <Field opened nearMines={3} />
+            <Field opened nearMines={6} />
         </SafeAreaView>
     );
 }
